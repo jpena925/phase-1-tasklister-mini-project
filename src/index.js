780872ef8c826bf3695f210ad9fa5db328957ffc
Jack Pena <penajackc@gmail.com>
2022-01-17 12:01:28 -0700

    Working Task List

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('submit', (e) =>{
+    e.preventDefault()
+    addToList(e.target["new-task-description"].value)
+    form.reset()
+  })
 });
+
+
+function addToList(newTask){
+  let listItem = document.createElement('p')
+  let itemButton = document.createElement('button')
+  itemButton.addEventListener('click', deleteTask)
+  listItem.textContent = `${newTask} `
+  itemButton.textContent = 'x '
+  listItem.appendChild(itemButton)
+  document.querySelector('#tasks').appendChild(listItem)
+}
+
+function deleteTask(event){
+  event.target.parentNode.remove()
+}
